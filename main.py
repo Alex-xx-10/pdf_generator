a77659546a3a670068d3cc63053c2149ef3dbf1a
Alex <arkadiuszk014@gmail.com>
2023-08-21 17:02:06 +0200

    Add lines

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # Set the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    range_main = range(21, 290, 10)
+
+    for r in range_main:
+        pdf.line(10, r, 200, r)
 
     # Set the footer
     pdf.ln(265)
@@ -21,9 +26,13 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        range_main = range(21, 290, 10)
+
+        for r in range_main:
+            pdf.line(10, r, 200, r)
 
         # Set the footer
         pdf.ln(277)
